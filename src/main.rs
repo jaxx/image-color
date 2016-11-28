1cf296a316da3e822706b4395e02c7014f730da6
Jakko Sikkar <jakko.sikkar@mail.com>
2016-11-28 23:19:35 +0200

    simplify code

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,9 @@ use image::{DynamicImage, GenericImage, Pixel, Rgb};
 
 fn main() {
     let image = load_image(env::args()).unwrap();
-    let color_map = Arc::new(Mutex::new(HashMap::new()));
+    let dominant_color = process_image_parallel(&image);
 
-    process_image_parallel(&image, &color_map);
-
-    println!("{}", get_dominant_color(&(*color_map.lock().unwrap())));
+    println!("{}", dominant_color);
 }
 
 fn load_image(mut arguments: Args) -> Result<DynamicImage, String> {
@@ -22,19 +20,30 @@ fn load_image(mut arguments: Args) -> Result<DynamicImage, String> {
         .and_then(|path| image::open(path).map_err(|e| e.to_string()))
 }
 
-fn process_image_parallel(image: &DynamicImage, color_map: &Arc<Mutex<HashMap<Rgb<u8>, u64>>>) {
+fn process_image_parallel(image: &DynamicImage) -> String {
     const NTHREADS: usize = 8;
     let mut thread_handles = vec![];
 
-    for _ in 0..NTHREADS {
-        let color_map_clone = color_map.clone();
-        let pixels = image.pixels().skip(NTHREADS * 1000).take(1000).collect();
+    let color_map: Arc<Mutex<HashMap<Rgb<u8>, u64>>> = Arc::new(Mutex::new(HashMap::new()));
 
-        thread_handles.push(spawn(move || process_pixels(pixels, color_map_clone)));
+    {
+        for _ in 0..NTHREADS {
+            let color_map_clone = color_map.clone();
+            let pixels = image.pixels().skip(NTHREADS * 1000).take(1000).collect();
+
+            thread_handles.push(spawn(move || process_pixels(pixels, color_map_clone)));
+        }
+
+        for handle in thread_handles {
+            handle.join().unwrap();
+        }
     }
 
-    for handle in thread_handles {
-        handle.join().unwrap();
+    let data = color_map.lock().unwrap();
+
+    match data.iter().max_by_key(|&(_, &count)| count) {
+        Some((&color, _)) => format!("#{:X}{:X}{:X}", color.data[0], color.data[1], color.data[2]),
+        None => String::new(),
     }
 }
 
@@ -42,16 +51,9 @@ fn process_pixels(pixels: Vec<(u32, u32, image::Rgba<u8>)>,
                   color_map: Arc<Mutex<HashMap<Rgb<u8>, u64>>>) {
     for (_, _, pixel) in pixels {
         let pixel_rgb = pixel.to_rgb();
-
         let mut data = color_map.lock().unwrap();
+
         let counter = data.entry(pixel_rgb).or_insert(0);
         *counter += 1;
-    }
-}
-
-fn get_dominant_color(color_map: &HashMap<Rgb<u8>, u64>) -> String {
-    match color_map.iter().max_by_key(|&(_, &count)| count) {
-        Some((&color, _)) => format!("#{:X}{:X}{:X}", color.data[0], color.data[1], color.data[2]),
-        None => String::new(),
     }
 }
