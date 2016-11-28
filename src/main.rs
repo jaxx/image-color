414beff17e50d6f703d12a3fac82ec2f5eeb3128
Jakko Sikkar <jakko.sikkar@mail.com>
2016-11-28 22:38:31 +0200

    trying some threading

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ extern crate image;
 
 use std::env::{self, Args};
 use std::collections::HashMap;
+use std::thread::spawn;
+use std::sync::{Arc, Mutex};
 
 use image::{DynamicImage, GenericImage, Pixel, Rgb};
 
 fn main() {
     let image = load_image(env::args()).unwrap();
-    let pixels = load_pixels(&image);
-    let dominating_rgb = get_dominating_rgb(pixels).unwrap();
+    let color_map = Arc::new(Mutex::new(HashMap::new()));
 
-    println!("{}", rgb_to_hex(dominating_rgb));
+    process_image_parallel(&image, &color_map);
+
+    println!("{}", get_dominant_color(&(*color_map.lock().unwrap())));
 }
 
 fn load_image(mut arguments: Args) -> Result<DynamicImage, String> {
@@ -19,29 +22,37 @@ fn load_image(mut arguments: Args) -> Result<DynamicImage, String> {
         .and_then(|path| image::open(path).map_err(|e| e.to_string()))
 }
 
-fn load_pixels(image: &DynamicImage) -> Vec<Rgb<u8>> {
-    let mut result = Vec::new();
+fn process_image_parallel(image: &DynamicImage, color_map: &Arc<Mutex<HashMap<Rgb<u8>, u64>>>) {
+    const NTHREADS: usize = 8;
+    let mut thread_handles = vec![];
 
-    for x in 0..image.width() {
-        for y in 0..image.height() {
-            result.push(image.get_pixel(x, y).to_rgb());
-        }
+    for _ in 0..NTHREADS {
+        let color_map_clone = color_map.clone();
+        let pixels = image.pixels().skip(NTHREADS * 1000).take(1000).collect();
+
+        thread_handles.push(spawn(move || process_pixels(pixels, color_map_clone)));
     }
 
-    result
+    for handle in thread_handles {
+        handle.join().unwrap();
+    }
 }
 
-fn get_dominating_rgb(pixels: Vec<Rgb<u8>>) -> Option<Rgb<u8>> {
-    let mut color_map = HashMap::new();
+fn process_pixels(pixels: Vec<(u32, u32, image::Rgba<u8>)>,
+                  color_map: Arc<Mutex<HashMap<Rgb<u8>, u64>>>) {
+    for (_, _, pixel) in pixels {
+        let pixel_rgb = pixel.to_rgb();
 
-    for pixel in pixels {
-        let counter = color_map.entry(pixel).or_insert(0);
+        let mut data = color_map.lock().unwrap();
+        let counter = data.entry(pixel_rgb).or_insert(0);
         *counter += 1;
     }
+}
 
+fn get_dominant_color(color_map: &HashMap<Rgb<u8>, u64>) -> String {
     match color_map.iter().max_by_key(|&(_, &count)| count) {
-        Some((&color, _)) => Some(color),
-        None => None,
+        Some((&color, _)) => rgb_to_hex(color),
+        None => String::new(),
     }
 }
 
